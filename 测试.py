9a8c26bb22a78fd27edf21646e48ba1b79dc329f
yc <2386302421@qq.com>
2024-10-26 11:16:01 +0800

    Initial commit

---
+++ 测试.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# // 设定3列
+col1, col2, col3 = st.columns(3)
+
+# // 设定不同的列标题和展示的内容
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
