540cf5c847edce96c494b98b1d96b07351201380
yc <2386302421@qq.com>
2024-10-26 17:02:01 +0800

    Initial commit

---
+++ 爱心代码.py
@@ -0,0 +1,7 @@
+# coding:utf-8
+import streamlit as st
+from streamlit_extras.let_it_rain import rain
+# 爱心代码 ，伴有动画效果，爱心下落
+st.balloons()
+rain(emoji="💕", font_size=54,
+     falling_speed=5, animation_length="infinite", )
